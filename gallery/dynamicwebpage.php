a2a265a75ec6030d522fc702b1db30a3e32d6d7d
Nikolay Kolibarov <92979997+NikolayKolinarov@users.noreply.github.com>
2023-02-26 13:51:29 +0200

    Updated

--- gallery/dynamicwebpage.php
+++ gallery/dynamicwebpage.php
@@ -18,7 +18,7 @@
     />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="galleryStyle.css?<?php echo time();?>">
-    <title><?php echo $_SESSION["drawingName"];?></title>
+    <title><?php echo $_SESSION["Name"];?></title>
 </head>
 <body>
     <main>
@@ -45,21 +45,21 @@
       $username = "root";
       $password = "";
       $dbname = "thelouvre";
-      $drawingID = $_GET["id"];
+      $drawingId = $_GET["Id"];
 
       $conn = new mysqli($servername, $username, $password, $dbname);
-      $sql = "SELECT * FROM first_floor WHERE ID=$drawingID";
+      $sql = "SELECT * FROM exhibits WHERE Id=$drawingId";
       $result = $conn->query($sql);
 
       if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-          $_SESSION["drawingName"] = $row["NAME"];
+          $_SESSION["drawingName"] = $row["Name"];
           echo '<div class="col">
                       <div class="card shadow-sm h-10">
-                        <img class="mx-auto" style="width: 50%; height: 50%; align: center;" src="'. $row["IMAGE"] . '">
+                        <img class="mx-auto" style="width: 50%; height: 50%; align: center;" src="'. $row["FilePath"] . '">
                         <div class="card-body text-center">
                         <form method="get" action="dynamicwebpage.php" class="mx-auto">
-                          <h2 class="card-text text-center my-2"><button class="buton border-0 bg-black text-white" type="submit" name="id" value="'. $row["ID"] .'" class="nav-link text-dark"</button>' . $row["NAME"] . '</h2>
+                          <h2 class="card-text text-center my-2"><button class="buton border-0 bg-black text-white" type="submit" name="Id" value="'. $row["Id"] .'" class="nav-link text-dark"</button>' . $row["Name"] . '</h2>
                         </form>                 
                         </div>
                       </div>
---
+++ register/logIn.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/bootstrap/css/main.css" />
+    <link
+      rel="stylesheet"
+      href="/node_modules/bootstrap/dist/css/boostrap.min.css"
+    />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+      crossorigin="anonymous"
+    />
+    <title>Sign in</title>
+    <style>
+        form {
+            margin-top: 100px;
+        }
+
+       .btn-login {
+        font-size: 0.9rem;
+        letter-spacing: 0.05rem;
+        padding: 0.75rem 1rem;
+      }
+    </style>
+  </head>
+  <body>
+    <?php
+    //unset($_SESSION["registerErrors"]);
+    if(isset($_SESSION["registerErrors"])){
+      //var_dump($_SESSION["registerErrors"]);
+      for($i = 0; $i < count($_SESSION["registerErrors"]); $i++){
+        $currentError = $_SESSION["registerErrors"][$i];
+        echo "<p>" . $currentError . "</p>";
+      }
+    }
+    unset($_SESSION["registerErrors"]);  
+    ?>
+    <form method="get" action="loginProcess.php">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div class="card border-0 shadow rounded-3 my-5">
+              <div class="card-body p-4 p-sm-5">
+                <h5 class="card-title text-center mb-5 fw-light fs-5">
+                  Log In
+                </h5>
+
+                <div class="form-floating mb-3">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="text"
+                    name="email"
+                    value="<?= @$email ?>"
+                  />
+                  <label for="text">Email address</label>
+                </div>
+                <div class="form-floating mb-3">
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="text"
+                    name="password"
+                    value="<?= @$password ?>"
+                  />
+                  <label for="text">Password</label>
+                </div>
+
+                <div class="d-grid">
+                  <input
+                    class="btn btn-primary btn-login text-uppercase fw-bold"
+                    type="submit"
+                    name="submit"
+                    value="Sign in"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+
+    <script src="node_modules/bootstrap/dist/js/boostrap.bundle.min.js"></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
